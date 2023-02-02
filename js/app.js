2f5affe3573a3b74dbd793904106aedd2703f126
WVProgrammer <47639410+wvprogram@users.noreply.github.com>
2023-02-01 20:25:31 -0600

    update to index and js file

--- js/app.js
+++ js/app.js
@@ -6,7 +6,10 @@ let init = () => {
     current = 1,
     time = 6000;
 
+    //add animation class to slide
     slides[0].classList.add("jumbo-slider__slide--active");
+
+    links[current-1].classList.add("jumbo-slider__link--active");
 }
 
 init();
