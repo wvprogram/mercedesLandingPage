7a1fe6a4f690b589b53cb47d54d54c922f696db1
WVProgrammer <47639410+wvprogram@users.noreply.github.com>
2023-02-01 22:37:05 -0600

    update to index and js file

--- js/app.js
+++ js/app.js
@@ -7,9 +7,42 @@ let init = () => {
     time = 6000;
 
     //add animation class to slide
-    slides[0].classList.add("jumbo-slider__slide--active");
-
+    slides[current-1].classList.add("jumbo-slider__slide--active");
     links[current-1].classList.add("jumbo-slider__link--active");
+    circles[current-1].classList.add('jumbo-slider__circle--active');
+
+    //update elipsis and name links
+    let updateNav = (current) => {
+        // console.log(`current: ${current}`);
+        for (let index = 0; index < slides.length; index++) {
+            links[index].classList.remove("jumbo-slider__link--active");
+            circles[index].classList.remove("jumbo-slider__circle--active");
+        }
+
+        links[current-1].classList.add("jumbo-slider__link--active");
+        circles[current-1].classList.add("jumbo-slider__circle--active");
+    }
+
+    let startSliding = () => {
+        setInterval(() => {
+            // console.log(`current: ${current}`);
+            slides[1].classList.add("jumbo-slider__slide--active");
+            slides[0].classList.remove("jumbo-slider__slide--active");
+
+            //makes a copy of all the child elements inside container of ul
+            container.appendChild(slides[0].cloneNode([true]));
+            container.removeChild(slides[0]);
+
+            if (current < slides.length) {
+                current++;
+                updateNav(current);
+            } else {
+                current = 1;
+                updateNav(current);
+            }
+        }, time);
+    }
+    startSliding();
 }
 
 init();
